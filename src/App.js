1085af6bc5cb777903a0a5ef1e2a91b8182f8482
surajverma2587 <surajverma2587@gmail.com>
2023-01-16 18:50:14 +0000

    complete form with error handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { Banner } from "./components/Banner";
 import { CalculatorForm } from "./components/CalculatorForm";
 import { Results } from "./components/Results";
 
 export const App = () => {
+  const [error, setError] = useState(false);
+
   return (
     <div className="container">
       <Banner />
-      <CalculatorForm />
+      <CalculatorForm setError={setError} />
+      {error && (
+        <div className="alert alert-danger mt-4" role="alert">
+          Please complete the form!!
+        </div>
+      )}
       <Results />
     </div>
   );
--- src/components/CalculatorForm.js
+++ src/components/CalculatorForm.js
@@ -1,3 +1,66 @@
-export const CalculatorForm = () => {
-  return <div>CalculatorForm</div>;
+import { useState } from "react";
+
+export const CalculatorForm = ({ setError }) => {
+  const [taxYear, setTaxYear] = useState("");
+  const [annualSalary, setAnnualSalary] = useState();
+
+  const handleChangeTaxYear = (event) => {
+    setTaxYear(event.target.value);
+  };
+
+  const handleChangeSalary = (event) => {
+    setAnnualSalary(parseFloat(event.target.value));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (taxYear && annualSalary) {
+      setError(false);
+      // calculate the take home salary
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <form className="p-4 bg-light" onSubmit={handleSubmit}>
+      <div className="my-3">
+        <label htmlFor="taxYear" className="form-label text-dark">
+          Select Tax Year
+        </label>
+        <select
+          className="form-select"
+          id="taxYear"
+          onChange={handleChangeTaxYear}
+          defaultValue={taxYear}
+        >
+          <option value="" disabled>
+            Select a tax year
+          </option>
+          <option value="2021-2022">2021-2022</option>
+          <option value="2022-2023">2022-2023</option>
+        </select>
+      </div>
+
+      <div className="my-3">
+        <label htmlFor="annualSalary" className="form-label text-dark">
+          Gross Annual Salary
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="annualSalary"
+          placeholder="Enter your gross annual salary"
+          onChange={handleChangeSalary}
+        />
+      </div>
+
+      <div className="mt-4 text-center">
+        <button className="btn btn-success" type="submit">
+          Calculate
+        </button>
+      </div>
+    </form>
+  );
 };
